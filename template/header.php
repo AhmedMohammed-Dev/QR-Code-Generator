c1422883064943861a72d096f291858cd0addfd5
AhmedMohammed <121712221+AhmedMohammed-Dev@users.noreply.github.com>
2024-11-12 18:34:46 -0300

    Ahmed

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<div class="bg-primary position-relative">
+  <div class="overlay-gradient"></div>
+  <div class="container position-relative">
+  	<div class="row py-5">
+    	<div class="col">
+        <h1 class="display-1">QR Code Generator</h1>
+        <p><a href="https://github.com/AhmedMohammed-Dev" href="#" class="btn btn-primary btn-lg shadow" role="button" target="_blank">Learn more &raquo;</a></p>
+    </div>
+    </div>
+  </div>
+</div>
---
+++ template/navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar bg-primary m-0 navbar-expand-sm navbar-dark bg-dark">
+	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#qrcdrNavbar" aria-controls="qrcdrNavbar" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="qrcdrNavbar">
+		<ul class="navbar-nav ms-auto">
+
+			<?php echo qrcdr()->langMenu('menu'); ?>
+		</ul>
+	</div>
+</nav>
